adc79a34d14caaedd30912786dcb54ca669e1a07
plastered_crab <jeffw773@gmail.com>
2025-01-04 02:52:00 -0800

    Gordon Ramsey is with me like the movie Ghost

--- common/src/main/kotlin/com/cobblemon/mod/common/client/gui/cookingpot/CookingPotResultSlot.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/client/gui/cookingpot/CookingPotResultSlot.kt
@@ -20,7 +20,7 @@ class CookingPotResultSlot(
 ) : Slot(container, index, x, y) {
 
     override fun onTake(player: Player, stack: ItemStack) {
-        super.onTake(player, stack)
+        val menu = player.containerMenu
 
         // Variables to store bait and seasoning
         var bait1 = FishingBait.BLANK_BAIT
@@ -30,45 +30,54 @@ class CookingPotResultSlot(
         var seasoning2 = Seasoning.BLANK_SEASONING
         var seasoning3 = Seasoning.BLANK_SEASONING
 
-        // Iterate through slots 10-12
-        for ((index, slot) in (10..12).withIndex()) {
-            val itemInSlot = container.getItem(slot)
+        if (menu is CookingPotMenu) {
+            // Iterate through slots 10-12
+            for ((index, slot) in (10..12).withIndex()) {
+                val itemInSlot = menu.getSlot(slot).item
 
-            if (!itemInSlot.isEmpty) {
-                // Check if item is a bait
-                val bait = FishingBaits.getFromBaitItemStack(itemInSlot)
-                if (bait != null) {
-                    when (index) {
-                        0 -> bait1 = bait
-                        1 -> bait2 = bait
-                        2 -> bait3 = bait
+                if (!itemInSlot.isEmpty) {
+                    // Check if item is a bait
+                    val bait = FishingBaits.getFromBaitItemStack(itemInSlot)
+                    if (bait != null) {
+                        when (index) {
+                            0 -> bait1 = bait
+                            1 -> bait2 = bait
+                            2 -> bait3 = bait
+                        }
                     }
-                }
 
-                // Check if item is a seasoning
-                val seasoning = Seasonings.getFromItemStack(itemInSlot)
-                if (seasoning != null) {
-                    when (index) {
-                        0 -> seasoning1 = seasoning
-                        1 -> seasoning2 = seasoning
-                        2 -> seasoning3 = seasoning
+                    // Check if item is a seasoning
+                    val seasoning = Seasonings.getFromItemStack(itemInSlot)
+                    if (seasoning != null) {
+                        when (index) {
+                            0 -> seasoning1 = seasoning
+                            1 -> seasoning2 = seasoning
+                            2 -> seasoning3 = seasoning
+                        }
                     }
                 }
             }
+
+            // Create CookingComponent and attach to result item
+            val cookingComponent = CookingComponent(
+                bait1 = bait1,
+                bait2 = bait2,
+                bait3 = bait3,
+                seasoning1 = seasoning1,
+                seasoning2 = seasoning2,
+                seasoning3 = seasoning3
+            )
+
+            // Attach the CookingComponent to the result stack
+            stack.set(CobblemonItemComponents.COOKING_COMPONENT, cookingComponent)
+
+            menu.consumeCraftingIngredients() // Decrement ingredients
+            menu.broadcastChanges() // Notify the client
+        } else {
+            println("Player menu is not CookingPotMenu!")
         }
 
-        // Create CookingComponent and attach to result item
-        val cookingComponent = CookingComponent(
-            bait1 = bait1,
-            bait2 = bait2,
-            bait3 = bait3,
-            seasoning1 = seasoning1,
-            seasoning2 = seasoning2,
-            seasoning3 = seasoning3
-        )
-
-        // Attach the CookingComponent to the result stack
-        stack.set(CobblemonItemComponents.COOKING_COMPONENT, cookingComponent)
+        super.onTake(player, stack)
     }
 
     override fun mayPlace(stack: ItemStack): Boolean {
