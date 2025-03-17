de042f71ddd6c526a2f5db1252f4a8c887915768
WhiteFenix19 <30911462+WhiteFenix19@users.noreply.github.com>
2025-03-17 12:18:28 -0400

    Updated command logic to improve legibility

--- common/src/main/kotlin/com/cobblemon/mod/common/command/ChangePCBoxesCommand.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/command/ChangePCBoxesCommand.kt
@@ -7,94 +7,90 @@ import com.cobblemon.mod.common.util.*
 import com.mojang.brigadier.Command
 import com.mojang.brigadier.CommandDispatcher
 import com.mojang.brigadier.arguments.IntegerArgumentType
-import com.mojang.brigadier.arguments.StringArgumentType
 import com.mojang.brigadier.context.CommandContext
 import net.minecraft.commands.CommandSourceStack
 import net.minecraft.commands.Commands.argument
 import net.minecraft.commands.Commands.literal
 import net.minecraft.commands.arguments.EntityArgument
-import net.minecraft.server.level.ServerPlayer
+import net.minecraft.commands.arguments.selector.EntitySelector
 
 object ChangePCBoxesCommand {
     private const val NAME = "boxcount"
     fun register(dispatcher : CommandDispatcher<CommandSourceStack>) {
         dispatcher.register(literal(NAME)
                 .permission(CobblemonPermissions.CHANGE_BOX_COUNT)
-                .then(argument("action", StringArgumentType.word())
-                    .suggests { _, builder ->
-                        builder
-                            .suggest("add")
-                            .suggest("remove")
-                            .suggest("set")
-                            .suggest("query")
-                        builder.buildFuture()
-                    }
-                    .then(argument("player", EntityArgument.player())
-                        .executes {
-                            context ->
-                            val player = context.player()
-                            val action = StringArgumentType.getString(context, "action")
-                            val number = 1
-                            execute(context, player, action, number)
-                        }
-                        .then(argument("number", IntegerArgumentType.integer(1, 1000))
-                            .executes {
-                                context ->
-                                val player = context.player()
-                                val action = StringArgumentType.getString(context, "action")
-                                val number = IntegerArgumentType.getInteger(context, "number")
-                                execute(context, player, action, number)
-                            } ))))
-
+                .then(argument("player", EntityArgument.player())
+                    .then(literal("query").executes(::executeQuery))
+                    .then(literal("add").then(
+                        argument("amount", IntegerArgumentType.integer(1, 1000)).executes(::executeAdd)
+                    ))
+                    .then(literal("remove").then(
+                        argument("amount", IntegerArgumentType.integer(1, 1000)).executes(::executeRemove)
+                    ))
+                    .then(literal("set").then(
+                        argument("amount", IntegerArgumentType.integer(1, 1000)).executes(::executeSet)
+                    ))))
     }
 
 
 
-    private fun execute(
-        context: CommandContext<CommandSourceStack>,
-        player:  ServerPlayer,
-        action: String,
-        number: Int
-    ): Int {
+
+    private fun executeQuery(context: CommandContext<CommandSourceStack>): Int {
+        val player = context.getArgument("player", EntitySelector::class.java).findSinglePlayer(context.source)
         val playerPc = player.pc()
+        context.source.sendSystemMessage((lang("command.boxcount", player.name, playerPc.boxes.size)))
 
-        when(action) {
-            "add" -> {
-                playerPc.resize(playerPc.boxes.size + number, true)
-                playerPc.sendTo(player)
-                context.source.sendSystemMessage((lang("command.changeboxcount", player.name, playerPc.boxes.size).green()))
-            }
-            "remove" -> {
-                val slicedBoxes = playerPc.boxes.slice(playerPc.boxes.size - number until playerPc.boxes.size)
-                val boxEmpty = slicedBoxes.all { box -> box.getNonEmptySlots().isEmpty() }
+        return Command.SINGLE_SUCCESS
+    }
+    private fun executeAdd(context: CommandContext<CommandSourceStack>): Int {
+        val player = context.getArgument("player", EntitySelector::class.java).findSinglePlayer(context.source)
+        val playerPc = player.pc()
+        val amount = context.getArgument("amount", Int::class.java)
 
-                if (boxEmpty) {
-                    playerPc.resize(playerPc.boxes.size - number, true)
-                    playerPc.sendTo(player)
-                    context.source.sendSystemMessage((lang("command.changeboxcount", player.name, playerPc.boxes.size).green()))
-                }
-                else {
-                    context.source.sendSystemMessage(lang("command.changeboxcount.removing_not_empty_box").red())
-                }
-            }
-            "set" -> {
-                if (number < playerPc.boxes.size) {
-                    val slicedBoxes = playerPc.boxes.slice(number until playerPc.boxes.size)
-                    val boxEmpty = slicedBoxes.all { box -> box.getNonEmptySlots().isEmpty() }
+        playerPc.resize(playerPc.boxes.size + amount, true)
+        playerPc.sendTo(player)
+        context.source.sendSystemMessage((lang("command.changeboxcount", player.name, playerPc.boxes.size).green()))
 
-                    if (!boxEmpty) {
-                        context.source.sendSystemMessage((lang("command.changeboxcount.removing_not_empty_box").red()))
-                        return Command.SINGLE_SUCCESS
-                    }
-                }
-                playerPc.resize(number, true)
-                playerPc.sendTo(player)
-                context.source.sendSystemMessage((lang("command.changeboxcount", player.name, playerPc.boxes.size).green()))
-            }
-            "query" -> {
-                context.source.sendSystemMessage((lang("command.boxcount", player.name, playerPc.boxes.size)))
+        return Command.SINGLE_SUCCESS
+    }
+    private fun executeRemove(context: CommandContext<CommandSourceStack>): Int {
+        val player = context.getArgument("player", EntitySelector::class.java).findSinglePlayer(context.source)
+        val playerPc = player.pc()
+        val amount = context.getArgument("amount", Int::class.java)
+
+        val slicedBoxes = playerPc.boxes.slice(playerPc.boxes.size - amount until playerPc.boxes.size)
+        val boxEmpty = slicedBoxes.all { box -> box.getNonEmptySlots().isEmpty() }
+
+        if (boxEmpty) {
+            playerPc.resize(playerPc.boxes.size - amount, true)
+            playerPc.sendTo(player)
+            context.source.sendSystemMessage((lang("command.changeboxcount", player.name, playerPc.boxes.size).green()))
+        }
+        else {
+            context.source.sendSystemMessage(lang("command.changeboxcount.removing_not_empty_box").red())
+            return 0
+        }
+
+        return Command.SINGLE_SUCCESS
+    }
+    private fun executeSet(context: CommandContext<CommandSourceStack>): Int {
+        val player = context.getArgument("player", EntitySelector::class.java).findSinglePlayer(context.source)
+        val playerPc = player.pc()
+        val amount = context.getArgument("amount", Int::class.java)
+
+        val slicedBoxes = playerPc.boxes.slice(playerPc.boxes.size - amount until playerPc.boxes.size)
+        val boxEmpty = slicedBoxes.all { box -> box.getNonEmptySlots().isEmpty() }
+
+        if (amount < playerPc.boxes.size) {
+            if (!boxEmpty) {
+                context.source.sendSystemMessage((lang("command.changeboxcount.removing_not_empty_box").red()))
+                return 0
             }
         }
+        playerPc.resize(amount, true)
+        playerPc.sendTo(player)
+        context.source.sendSystemMessage((lang("command.changeboxcount", player.name, playerPc.boxes.size).green()))
+
         return Command.SINGLE_SUCCESS
     }
 }
