08633c223e8a0f23cffaabe643cbe032060c4ebf
Plastered_Crab <49732785+PlasteredCrab@users.noreply.github.com>
2025-04-04 15:50:56 -0700

    Example code for combining similar bait effects. Needs review if we want to do this.

--- common/src/main/kotlin/com/cobblemon/mod/common/item/crafting/BaitSeasoningProcessor.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/item/crafting/BaitSeasoningProcessor.kt
@@ -9,6 +9,7 @@
 package com.cobblemon.mod.common.item.crafting
 
 import com.cobblemon.mod.common.CobblemonItemComponents
+import com.cobblemon.mod.common.api.fishing.SpawnBait
 import com.cobblemon.mod.common.api.fishing.SpawnBaitEffects
 import com.cobblemon.mod.common.item.components.BaitEffectsComponent
 import net.minecraft.resources.ResourceLocation
@@ -21,6 +22,35 @@ object BaitSeasoningProcessor : SeasoningProcessor {
         for (seasoningStack in seasoning) {
             baitIdentifiers.addAll(SpawnBaitEffects.getBaitIdentifiersFromItem(seasoningStack.itemHolder))
         }
+
+        val rawEffects = baitIdentifiers
+                .mapNotNull { SpawnBaitEffects.getFromIdentifier(it) }
+                .flatMap { it.effects }
+
+        val combinedEffects = combineBaitEffects(rawEffects) // TODO something like this would possibly combine similar bait effects before applying it to the stack. But the Component data would have to change to not just be ResourceLocations
+
         result.set(CobblemonItemComponents.BAIT_EFFECTS, BaitEffectsComponent(baitIdentifiers.toList()))
+    }
+
+    fun combineBaitEffects(effects: List<SpawnBait.Effect>): List<SpawnBait.Effect> {
+        val grouped = effects.groupBy { Pair(it.type, it.subcategory) }
+
+        return grouped.map { (key, effects) ->
+            val count = effects.size
+            val totalChance = effects.sumOf { it.chance }
+            val totalValue = effects.sumOf { it.value }
+
+            val multiplier = when (count) {
+                2 -> 0.8
+                3 -> 0.9
+                else -> 1.0
+            }
+
+            val adjustedChance = (totalChance * multiplier).coerceAtMost(1.0)
+            val adjustedValue = totalValue * multiplier
+
+            val (type, subcategory) = key
+            SpawnBait.Effect(type, subcategory, adjustedChance, adjustedValue)
+        }
     }
 }
