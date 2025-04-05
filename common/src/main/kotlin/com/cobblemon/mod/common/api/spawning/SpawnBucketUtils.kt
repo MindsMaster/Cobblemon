4eb268c4afad559157c40b923003f61284c1c8a9
plastered_crab <jeffw773@gmail.com>
2025-04-05 12:23:02 -0700

    More tweaks to the rarity bucket effect

--- common/src/main/kotlin/com/cobblemon/mod/common/api/spawning/SpawnBucketUtils.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/api/spawning/SpawnBucketUtils.kt
@@ -6,7 +6,7 @@ import com.cobblemon.mod.common.util.weightedSelection
 object SpawnBucketUtils {
     fun chooseAdjustedSpawnBucket(buckets: List<SpawnBucket>, bucketLureStrength: Int): SpawnBucket {
         val baseValues = listOf(91.6F, 7.0F, 1.0F, 0.4F)
-        val adjustments = listOf(-5.5F, 3.5F, 1.2F, 0.8F)
+        val adjustments = listOf(-6.9F, 3.6F, 2.3F, 1F)
 
         val adjustedWeights = buckets.mapIndexed { index, bucket ->
             if (index >= baseValues.size) {
