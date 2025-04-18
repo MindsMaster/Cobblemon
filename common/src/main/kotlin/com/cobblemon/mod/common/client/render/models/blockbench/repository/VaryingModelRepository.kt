6f6f9b088e1f821192c9473f7b906114fc42e915
FrankTheFarmer <frankthefarmeremail@gmail.com>
2025-04-18 19:40:03 +1000

    Ursaring remodel, locators, and temp animations i stole from charizard

--- common/src/main/kotlin/com/cobblemon/mod/common/client/render/models/blockbench/repository/VaryingModelRepository.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/client/render/models/blockbench/repository/VaryingModelRepository.kt
@@ -643,7 +643,6 @@ object VaryingModelRepository {
         inbuilt("gurdurr", ::GurdurrModel)
         inbuilt("conkeldurr", ::ConkeldurrModel)
         inbuilt("clodsire", ::ClodsireModel)
-        inbuilt("ursaring", ::UrsaringModel)
         inbuilt("litwick", ::LitwickModel)
         inbuilt("lampent", ::LampentModel)
         inbuilt("chandelure", ::ChandelureModel)
