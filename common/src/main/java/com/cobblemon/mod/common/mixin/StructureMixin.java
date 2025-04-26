e0e30e7e11c349c72eeaf1921754b2c07a82557a
Aram <schonhagearam@gmail.com>
2025-04-26 14:00:09 +0200

    Updated the Structure Check to also take StructurePieces into account

--- common/src/main/java/com/cobblemon/mod/common/mixin/StructureMixin.java
+++ common/src/main/java/com/cobblemon/mod/common/mixin/StructureMixin.java
@@ -1,7 +1,6 @@
 package com.cobblemon.mod.common.mixin;
 
 import com.cobblemon.mod.common.world.CobblemonStructureIDs;
-import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
 import net.minecraft.core.RegistryAccess;
 import net.minecraft.core.registries.Registries;
@@ -13,6 +12,7 @@ import net.minecraft.world.level.biome.BiomeSource;
 import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.minecraft.world.level.levelgen.RandomState;
 import net.minecraft.world.level.levelgen.structure.Structure;
+import net.minecraft.world.level.levelgen.structure.StructurePiece;
 import net.minecraft.world.level.levelgen.structure.StructureStart;
 import net.minecraft.world.level.levelgen.structure.pieces.StructurePiecesBuilder;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplateManager;
@@ -22,10 +22,10 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.Optional;
-import java.util.function.Predicate;
-import java.util.Set;
 import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Predicate;
 
 @Mixin(Structure.class)
 public abstract class StructureMixin {
@@ -47,14 +47,17 @@ public abstract class StructureMixin {
         Structure.GenerationContext generationContext = new Structure.GenerationContext(registryAccess, chunkGenerator, biomeSource, randomState, structureTemplateManager, seed, chunkPos, heightAccessor, validBiome);
         Optional<Structure.GenerationStub> optional = this.findValidGenerationPoint(generationContext);
         if (optional.isPresent()) {
-            BlockPos position = optional.get().position();
-
             ResourceLocation structureKey = registryAccess.registryOrThrow(Registries.STRUCTURE).getKey((Structure) (Object) this);
 
-            // Modify the 65 if you want to change the Y restriction
-            if (RESTRICTED_STRUCTURES.contains(structureKey) && position.getY() < 65) {
-                cir.setReturnValue(StructureStart.INVALID_START);
-                return;
+            // Check if the structure is restricted and validate all components
+            if (RESTRICTED_STRUCTURES.contains(structureKey)) {
+                StructurePiecesBuilder structurePiecesBuilder = optional.get().getPiecesBuilder();
+                for (StructurePiece piece : structurePiecesBuilder.build().pieces()) {
+                    if (piece.getBoundingBox().minY() < 65) {
+                        cir.setReturnValue(StructureStart.INVALID_START);
+                        return;
+                    }
+                }
             }
 
             StructurePiecesBuilder structurePiecesBuilder = optional.get().getPiecesBuilder();
