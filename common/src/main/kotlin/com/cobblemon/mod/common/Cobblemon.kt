b9824e5708d9bb4432aa88ca9ad2f3316a63cc92
plastered_crab <jeffw773@gmail.com>
2025-05-15 23:30:11 -0700

    Remove redundant coerce

--- common/src/main/kotlin/com/cobblemon/mod/common/Cobblemon.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/Cobblemon.kt
@@ -285,11 +285,6 @@ object Cobblemon {
             event.newFriendship = event.pokemon.friendship + increment.roundToInt()
         }
 
-        CobblemonEvents.FULLNESS_UPDATED.subscribe(Priority.LOWEST) { event ->
-            event.newFullness = event.newFullness.coerceAtMost(100)
-        }
-
-
         HeldItemProvider.register(CobblemonHeldItemManager, Priority.LOWEST)
     }
 
