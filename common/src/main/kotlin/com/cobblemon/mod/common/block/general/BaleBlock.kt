bbe8a6948a8bd8506fab54c2ea48e59c6323f638
Polymeta <40173118+Polymeta@users.noreply.github.com>
2025-06-05 14:32:52 +0100

    fix license, refactor saplingblock and make falldamagemultiplier configurable
    Signed-off-by: Polymeta <40173118+Polymeta@users.noreply.github.com>

--- common/src/main/kotlin/com/cobblemon/mod/common/block/general/BaleBlock.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/block/general/BaleBlock.kt
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2023 Cobblemon Contributors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 package com.cobblemon.mod.common.block.general
 
 import net.minecraft.core.BlockPos
@@ -7,8 +15,8 @@ import net.minecraft.world.level.block.RotatedPillarBlock
 import net.minecraft.world.level.block.state.BlockState
 
 
-class BaleBlock(properties: Properties) : RotatedPillarBlock(properties) {
+class BaleBlock(properties: Properties, val fallDamageMultiplier: Float = 0.3f) : RotatedPillarBlock(properties) {
     override fun fallOn(level: Level, state: BlockState, pos: BlockPos, entity: Entity, fallDistance: Float) {
-        entity.causeFallDamage(fallDistance, 0.3f, level.damageSources().fall())
+        entity.causeFallDamage(fallDistance, fallDamageMultiplier, level.damageSources().fall())
     }
 }
