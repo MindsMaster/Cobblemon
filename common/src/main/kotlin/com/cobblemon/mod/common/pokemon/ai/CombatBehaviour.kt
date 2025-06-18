991ec7cc1acd4cbab606817ae08dd4e619e02bea
plastered_crab <jeffw773@gmail.com>
2025-06-17 18:06:36 -0700

    species json to allow for deciding if mons flee or fight back when attacked

--- common/src/main/kotlin/com/cobblemon/mod/common/pokemon/ai/CombatBehaviour.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/pokemon/ai/CombatBehaviour.kt
@@ -13,11 +13,13 @@ import com.cobblemon.mod.common.api.molang.ObjectValue
 
 class CombatBehaviour {
     var willDefendSelf = false
+    var willFlee = false
     var willDefendOwner = true
 
     @Transient
     val struct = ObjectValue(this).also {
         it.addFunction("will_defend_self") { DoubleValue(willDefendSelf) }
+        it.addFunction("will_fee") { DoubleValue(willFlee) }
         it.addFunction("will_defend_owner") { DoubleValue(willDefendOwner) }
     }
 }
