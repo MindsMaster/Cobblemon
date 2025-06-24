9749acbb0c2bf5c1bb46b4b0765ca6d6d797c0d4
plastered_crab <jeffw773@gmail.com>
2025-06-24 08:29:17 -0700

    Fixes for behavior assignment of attacking hostile mobs

--- common/src/main/kotlin/com/cobblemon/mod/common/api/molang/MoLangFunctions.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/api/molang/MoLangFunctions.kt
@@ -1235,6 +1235,7 @@ object MoLangFunctions {
             map.put("is_sprinting") { DoubleValue(pokemonEntity.isUsingAltPose(cobblemonResource("sprinting"))) }
             map.put("in_air") { DoubleValue(pokemonEntity.isUsingAltPose(cobblemonResource("in_air"))) }
             map.put("is_wild") { DoubleValue(pokemonEntity.ownerUUID == null) }
+            map.put("is_in_party") { DoubleValue(pokemonEntity.pokemon.storeCoordinates.get()?.store is PartyStore) }
             map.put("is_ridden") { DoubleValue(pokemonEntity.hasControllingPassenger()) }
             map.put("has_aspect") { DoubleValue(it.getString(0) in pokemonEntity.aspects) }
             map.put("is_pokemon") { DoubleValue.ONE }
